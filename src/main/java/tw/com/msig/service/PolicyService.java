911d935d0c7615aa75705a107d3847dae7f554c2
50557 <50557@E0557.mingtai.com.tw>
2017-07-18 15:58:23 +0800

    est

--- src/main/java/tw/com/msig/service/PolicyService.java
+++ src/main/java/tw/com/msig/service/PolicyService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import tw.com.msig.dao.PolicyDao;
+import tw.com.msig.entity.Leave;
 import tw.com.msig.entity.Policy;
 
 /**
@@ -12,15 +13,26 @@ import tw.com.msig.entity.Policy;
  */
 public class PolicyService {
 
-  @Autowired
-  private PolicyDao dao;
+	@Autowired
+	private PolicyDao dao;
 
-  public List<Policy> getAll() {
-    return dao.findAll();
-  }
+	public List<Policy> getAll() {
+		return dao.findAll();
+	}
 
-  public void save(Policy policy) {
-    dao.insert(policy);
-  }
+	public void save(Policy policy) {
+		dao.insert(policy);
+	}
 
+	public void update(Policy policy) {
+		dao.update(policy);
+	}
+
+	public void delete(Policy policy) {
+		dao.delete(policy);
+	}
+
+	public Policy getOne(String id) {
+		return dao.findOne(id);
+	}
 }
