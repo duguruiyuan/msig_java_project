37faa0854dcea7db19dbf957a540d07e3097e80c
50547 <50547@E0547.mingtai.com.tw>
2017-07-18 15:21:27 +0800

    holiday

---
+++ src/main/java/tw/com/msig/dao/HolidayDao.java
@@ -0,0 +1,40 @@
+package tw.com.msig.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import tw.com.msig.entity.Holiday;
+
+public class HolidayDao {
+
+  @Autowired
+  private SessionFactory factory;
+
+  public List<Holiday> findAll() {
+    Session session = factory.getCurrentSession();
+    return (List<Holiday>) session.createQuery("from my_Holiday").list();
+  }
+
+  public void insert(Holiday holiday) {
+    factory.getCurrentSession().save(holiday);
+  }
+
+  public void update(Holiday holiday) {
+    factory.getCurrentSession().update(holiday);
+  }
+
+  public void delete(Holiday holiday) {
+    factory.getCurrentSession().update(holiday);
+  }
+    
+  public Holiday findOne(String id) {
+    Session session = factory.getCurrentSession();
+    return (Holiday) session.get(Holiday.class, id);
+  }
+
+  
+  
+}
