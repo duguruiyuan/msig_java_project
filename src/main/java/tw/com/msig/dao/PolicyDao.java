1762e9cae43a9ba3886b9347e040c521cd33c1ea
tim232385 <tim232385@gmail.com>
2017-07-18 14:21:42 +0800

    add findOne, findByCondition sample

--- src/main/java/tw/com/msig/dao/PolicyDao.java
+++ src/main/java/tw/com/msig/dao/PolicyDao.java
@@ -2,30 +2,51 @@ package tw.com.msig.dao;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import tw.com.msig.entity.Policy;
+import tw.com.msig.entity.Security;
 
 /**
  * @author Matt S.Y Ho
  */
 public class PolicyDao {
 
-    @Autowired
-    private SessionFactory factory;
+  @Autowired
+  private SessionFactory factory;
 
-    public List<Policy> findAll() {
-        Session session = factory.getCurrentSession();
-        return (List<Policy>) session.createQuery("from Policy").list();
-    }
+  public List<Policy> findAll() {
+    Session session = factory.getCurrentSession();
+    return (List<Policy>) session.createQuery("from Policy").list();
+  }
 
-    public void insert(Policy policy) {
-        factory.getCurrentSession().save(policy);
-    }
+  public void insert(Policy policy) {
+    factory.getCurrentSession().save(policy);
+  }
 
-    public void update(Policy policy){
-        factory.getCurrentSession().update(policy);
+  public void update(Policy policy) {
+    factory.getCurrentSession().update(policy);
+  }
+
+  public List<Policy> findByCondition(String policyNo, String applicantName) {
+    Session session = factory.getCurrentSession();
+    Criteria crti = (Criteria) session.createCriteria(Policy.class);
+    if (policyNo != null && !policyNo.isEmpty()) {
+      crti = crti.add(Restrictions.eq("policyNo", policyNo));
     }
+    if (applicantName != null && !applicantName.isEmpty()) {
+      crti = crti.add(Restrictions.like("applicantName", applicantName + "%"));
+    }
+    return crti.list();
+  }
+
+  public Policy findOne(String id) {
+    Session session = factory.getCurrentSession();
+    return (Policy) session.get(Security.class, id);
+  }
+
 }
--- src/main/java/tw/com/msig/dao/SecurityDao.java
+++ src/main/java/tw/com/msig/dao/SecurityDao.java
@@ -2,13 +2,10 @@ package tw.com.msig.dao;
 
 import java.util.List;
 
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import tw.com.msig.entity.Policy;
 import tw.com.msig.entity.Security;
 
 public class SecurityDao {
@@ -31,23 +28,6 @@ public class SecurityDao {
 
   public void delete(Security Security) {
     factory.getCurrentSession().update(Security);
-  }
-
-  public List<Policy> findByCondition(String policyNo, String applicantName) {
-    Session session = factory.getCurrentSession();
-    Criteria crti = (Criteria) session.createCriteria(Policy.class);
-    if (policyNo != null && !policyNo.isEmpty()) {
-      crti = crti.add(Restrictions.eq("policyNo", policyNo));
-    }
-    if (applicantName != null && !applicantName.isEmpty()) {
-      crti = crti.add(Restrictions.like("applicantName", applicantName + "%"));
-    }
-    return crti.list();
-  }
-
-  public Policy findOne(String id) {
-    Session session = factory.getCurrentSession();
-    return (Policy) session.get(Policy.class, id);
   }
 
 }
