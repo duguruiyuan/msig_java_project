f324517b2f4abbb70a0e31e047b70ad34878f263
Matt S.Y. Ho <methodho@gmail.com>
2017-07-25 16:39:03 +0800

    Update

--- src/main/java/tw/com/msig/web/IndexController.java
+++ src/main/java/tw/com/msig/web/IndexController.java
@@ -1,6 +1,5 @@
 package tw.com.msig.web;
 
-import tw.com.msig.service.PolicyService;
 import tw.com.msig.util.SecurityUtils;
 
 import javax.servlet.ServletException;
@@ -12,20 +11,16 @@ import java.io.IOException;
 @WebServlet("/")
 public class IndexController extends AbstractController {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 檢查是否有登入
-		if (SecurityUtils.requireLogin(req, resp)) {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
 
-			// 將參數吐回畫面上顯示(這邊的參數只能在下一個畫面用)
-			req.setAttribute("ip", req.getRemoteAddr());
+    // 將參數吐回畫面上顯示(這邊的參數只能在下一個畫面用)
+    req.setAttribute("ip", req.getRemoteAddr());
 
-			// 存在session內的參數會一直活著, jsp也拿得到所以不用特別塞
+    // 存在session內的參數會一直活著, jsp也拿得到所以不用特別塞
 
-			// 轉導到jsp上
-			forward(req, resp, "index");
-		}
-
-	}
-
+    // 轉導到jsp上
+    forward(req, resp, "index");
+  }
 }
