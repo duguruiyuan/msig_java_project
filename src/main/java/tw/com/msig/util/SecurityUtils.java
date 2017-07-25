d034a1a0a3f2b167017c65f229d46b365031949e
Matt S.Y. Ho <methodho@gmail.com>
2017-07-25 16:38:46 +0800

    Update

--- src/main/java/tw/com/msig/util/SecurityUtils.java
+++ src/main/java/tw/com/msig/util/SecurityUtils.java
@@ -1,6 +1,6 @@
 package tw.com.msig.util;
 
-import tw.com.msig.entity.User;
+import tw.com.msig.entity.Employee;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,35 +10,29 @@ import java.util.Objects;
 
 public class SecurityUtils {
 
-	public static final String SESSION_USER_DATA = "user_data";
-	public static final String LOGIN_URL = "login";
+  public static final String SESSION_USER_DATA = "user_data";
+  public static final String LOGIN_URL = "login";
 
-	/**
-	 * 檢查是否有登入, 若無登入則回到登入畫面
-	 */
-	public static boolean requireLogin(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		final HttpSession session = req.getSession();
-		if (Objects.isNull(session.getAttribute(SESSION_USER_DATA))) {
-			resp.sendRedirect(LOGIN_URL);
-			return false;
-		} else {
-			return true;
-		}
-	}
+  /** 檢查是否有登入, 若無登入則回到登入畫面 */
+  public static void requireLogin(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    final HttpSession session = req.getSession();
+    if (Objects.isNull(session.getAttribute(SESSION_USER_DATA))) {
+      resp.sendRedirect(LOGIN_URL);
+    }
+  }
 
-	/**
-	 * 將登入資料塞入
-	 */
-	public static void setLoginUser(HttpServletRequest req, User user) {
-		req.getSession().setAttribute(SecurityUtils.SESSION_USER_DATA, user);
-	}
+  /** 將登入資料塞入 */
+  public static void setLoginUser(HttpServletRequest req, Employee user) {
+    req.getSession().setAttribute(SESSION_USER_DATA, user);
+  }
 
-	/**
-	 * 取得目前登入者的資料
-	 */
-	public static User getLoginUser(HttpServletRequest req) {
-		final Object user = req.getSession().getAttribute(SESSION_USER_DATA);
-		return (User) user;
-	}
+  /** 取得目前登入者的資料 */
+  public static Employee getLoginUser(HttpServletRequest req) {
+    return (Employee) req.getSession().getAttribute(SESSION_USER_DATA);
+  }
 
+  public static void removeUser(HttpServletRequest req) {
+    req.getSession().removeAttribute(SESSION_USER_DATA);
+  }
 }
